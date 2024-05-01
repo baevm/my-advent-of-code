50c4eec9762db2c36cc0ca9e59ae8521aebb3114
baevm <baev2m@gmail.com>
2024-05-01 18:54:10 +0500

    2015: day 5 refactors

--- 2015/src/bin/05.rs
+++ 2015/src/bin/05.rs
@@ -1,5 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 use std::str;
+
+use regex::Regex;
 
 // https://adventofcode.com/2015/day/5
 advent_of_code::solution!(5);
@@ -8,37 +10,15 @@ pub fn part_one(input: &str) -> Option<u32> {
     let lines = input.lines();
     let mut nice_count = 0;
 
-    let forbidden: HashSet<String> = HashSet::from([
-        ("ab").to_owned(),
-        ("cd").to_owned(),
-        ("pq").to_owned(),
-        ("xy").to_owned(),
-    ]);
-    let vowels: HashSet<char> = HashSet::from_iter("aeiou".chars());
+    let forbidden = Regex::new(r"ab|cd|pq|xy").unwrap();
+    let vowels = Regex::new(r"[aeiou]").unwrap();
 
-    'outer: for line in lines {
-        let mut vowels_count = 0;
-        let mut is_double_found = false;
-        let mut last_ch: char = '\0';
+    for line in lines {
+        let vowels_count = vowels.find_iter(line).count();
+        let is_forbidden = forbidden.is_match(line);
+        let is_doubles = line.as_bytes().windows(2).any(|x| x[0] == x[1]);
 
-        for ch in line.chars() {
-            let last_plus_ch = last_ch.to_string() + &ch.to_string();
-            if forbidden.contains(&last_plus_ch) {
-                continue 'outer;
-            }
-
-            if vowels.contains(&ch) {
-                vowels_count += 1;
-            }
-
-            if last_ch == ch {
-                is_double_found = true;
-            }
-
-            last_ch = ch;
-        }
-
-        if vowels_count >= 3 && is_double_found {
+        if vowels_count > 2 && !is_forbidden && is_doubles {
             nice_count += 1;
         }
     }
@@ -54,41 +34,25 @@ pub fn part_two(input: &str) -> Option<u32> {
         // key: pair, value: ending index
         let mut pairs: HashMap<String, usize> = HashMap::new();
 
-        let mut is_triplet_found = false;
+        let is_triplet_found = line.as_bytes().windows(3).any(|x| x[0] == x[2]);
         let mut is_pair_found = false;
-        let mut triplet = "".to_string();
 
         let mut last_ch = '\0';
 
         for (i, ch) in line.chars().enumerate() {
             let last_plus_ch = last_ch.to_string() + &ch.to_string();
-
             let exist_pair = pairs.get(&last_plus_ch);
 
             match exist_pair {
                 Some(val) => {
                     if i - 1 != *val {
                         is_pair_found = true;
+                        break;
                     }
                 }
                 None => {
                     pairs.insert(last_plus_ch, i);
                 }
-            }
-
-            if triplet.len() < 3 {
-                triplet += &ch.to_string();
-            }
-
-            if triplet.len() == 3 {
-                let first = triplet.get(..=0).unwrap();
-                let third = triplet.get(2..).unwrap();
-
-                if first == third {
-                    is_triplet_found = true;
-                }
-
-                triplet.remove(0);
             }
 
             last_ch = ch;
